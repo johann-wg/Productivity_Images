1fbc387d184d744919d3ca17d67ce45fd34596d2
johann-wg <110111343+johann-wg@users.noreply.github.com>
2023-04-17 16:19:46 +0900

    Update image_textmining_kor.py

--- image_textmining_kor.py
+++ image_textmining_kor.py
@@ -9,12 +9,12 @@ custom_config = r'--oem 3 --psm 6 tessedit_char_whitelist=0123456789abcdefghijkl
 pytesseract.pytesseract.tesseract_cmd = r'/Tesseract-OCR/tesseract.exe'
 
 # 이미지 파일에서 텍스트 추출
-result = pytesseract.image_to_string('/Users/jjong/desktop/test/page_1.jpg', lang='kor', config=custom_config, output_type=Output.DICT)
+result = pytesseract.image_to_string('/Users/desktop/test/page_1.jpg', lang='kor', config=custom_config, output_type=Output.DICT)
 
 # 추출한 텍스트 출력
 doc = docx.Document()
 doc.add_paragraph(result['text'])
-doc.save('/Users/jjong/desktop/result.docx')
+doc.save('/Users/desktop/result.docx')
 
 # 텍스트 파일 출력
 # with open('result.txt', mode='w', encoding='utf-8') as f:
